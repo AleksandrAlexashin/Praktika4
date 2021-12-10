dd8b070ebf6e168b349b9c34aec708b520c76f76
AleksandrAlexashin <sashokmetall@mail.ru>
2021-12-11 00:31:53 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите любое число больше нуля ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int n1;
+            int s=0;
+            
+            for ( n1 = 1; n1<=n;n1++)
+            {
+                s += 2*n1-1;
+                Console.WriteLine("квадрат числа {0} равен {1} ", n1, s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
